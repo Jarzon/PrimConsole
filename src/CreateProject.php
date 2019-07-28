4ce4fe468082cc380530b90045bad7cbb0aac652
Jarzon <j@jasonvaillancourt.ca>
2019-07-28 16:09:26 -0400

    Copy config files in create project script too

--- src/CreateProject.php
+++ src/CreateProject.php
@@ -8,9 +8,12 @@ class CreateProject
 {
     public static function postCreateProject(Event $event)
     {
-        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        $root = $event->getComposer()->getConfig()->get('vendor-dir') . '../';
         $projectname = ucfirst(basename(realpath(".")));
 
-        FileHelper::replaceInFolder($vendorDir . '../', ['PrimBase', $projectname]);
+        FileHelper::copyFile($root . 'app/config/config.php.dist', $root . 'app/config/config.php');
+        FileHelper::copyFile($root . 'phinx.yml.dist', $root . 'phinx.yml');
+
+        FileHelper::replaceInFolder($root, ['PrimBase', $projectname]);
     }
 }
